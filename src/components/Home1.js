bff83554341a76cd7147b01c70dcac6dfee98d2b
Putra Ompusunggu <putraops@gmail.com>
2021-11-24 12:58:16 +0700

    Fixing home page

--- src/components/Home1.js
+++ src/components/Home1.js
@@ -13,17 +13,17 @@ class Home1 extends React.Component {
                         <div className="home-desc-center">
                             <div className="container">
                                 <Row className="align-items-center">
-                                    <Col lg="5">
+                                    <Col lg="6">
                                         <div className="mt-40 home-2-content">
-                                            <h2 className="text-white font-weight-normal home-2-title mb-0">Ambil Keputusan Investasi & Trading Lebih Cepat / Mudah</h2>
-                                            <p className="text-white-70 mt-4 f-15 mb-0">Ut enim ad minima veniam quis nostrum exercitationem ullam corporis at suscipit laboriosam nisi ut aliquid a commodi consequatur Quis autem.</p>
-                                            <div className="mt-5">
+                                            <h2 className="text-white font-weight-normal home-2-title f-22 mb-0">Ambil Keputusan Investasi & Trading Lebih Cepat dan Lebih Mudah</h2>
+                                            <p className="text-white-70 mt-4 f-18 mb-0">Mulai dari 2,000 / hari, bisa akses sepuasnya tanpa batas. Temukan Rekomendasi, riset dan analisa saham pilihan dari berbagai analis teknikal dan fundamental berpengalaman di Pasar Modal</p>
+                                            {/* <div className="mt-5">
                                                 <Link to="#" className="btn btn-custom mr-4">Learn More</Link>
-                                            </div>
+                                            </div> */}
                                         </div>
                                     </Col>
 
-                                    <Col lg="7">
+                                    <Col lg="6">
                                         <div className="mt-40 home-2-content position-relative">
                                             {/* <img src="images/home-2-img.png" alt="" className="img-fluid mx-auto d-block home-2-img mover-img" /> */}
                                             <div className="home-2-bottom-img">
